a5deb5b8ea0b4c294a8f3627a8e5b86a592f7733
Artur <metallist15431543@gmail.com>
2026-01-02 15:28:40 +0300

    clean

--- src/component/Checkbox.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Checkbox = () => {
-  return <input type='checkbox'></input>;
-};
-
-export default Checkbox;
--- src/component/Input.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Index = () => {
-  return <input></input>;
-};
-
-export default Index;
