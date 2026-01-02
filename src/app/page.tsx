ba05fb763b4a3bf9271b2263fb8858a2d12b6680
Artur <metallist15431543@gmail.com>
2026-01-02 15:28:29 +0300

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,5 @@
 "use client";
 
-import CalendarIcon from "@/icon/CalendarIcon";
-import { DatePicker } from "@/component/datPicker/datePicker";
-import { useState } from "react";
-import { DateRange } from "react-day-picker";
-
 export default function Home() {
-  const [selected, setSelected] = useState<Date | DateRange | undefined>();
-
-  const handleDateChange = (value: Date | DateRange | undefined) => {
-    setSelected(value);
-  };
-
-  return (
-    <div>
-      <DatePicker
-        label='Выбери дату'
-        mode='single'
-        value={selected}
-        onChange={handleDateChange}
-        minDate={new Date(2025, 0, 1)}
-        maxDate={new Date(2025, 11, 31)}
-      />
-      <div>
-        <DatePicker
-          label='Выбери дату'
-          mode='range'
-          value={selected}
-          onChange={handleDateChange}
-          minDate={new Date(2025, 0, 1)}
-          maxDate={new Date(2025, 11, 31)}
-        />
-      </div>
-    </div>
-  );
+  return <div>1</div>;
 }
